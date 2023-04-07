01a4a9aaa25816e5d5286d47b9e7714f82b6b283
Sergey An <ansergeyg@gmail.com>
2023-04-07 22:28:22 +0200

    Fix default params.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import * as gridLib from './grid.js';
 
 export function init(userConfig) {
-  let defaultConfig = {size: 111};
+  let defaultConfig = {
+    'canvasId': 'tutorial',
+    'width': 200,
+    'height': 200,
+    'cellCount': 30,
+    'cellSize': 20,
+    'sepDist': 1,
+};
   const config = Object.assign(defaultConfig, userConfig);
 
   document.addEventListener('DOMContentLoaded', (event) => {
 
-    const canvas = document.getElementById(config.canvasId);
+  const canvas = document.getElementById(config.canvasId);
 
     if (canvas.getContext) {
-      console.log(config);
       const ctx = canvas.getContext("2d");
       let grid = gridLib.buildGrid(config.cellSize, config.cellCount, config.sepDist);
       gridLib.drawGrid(ctx, grid, config.cellSize, config.cellCount);
